d57927d876b65ac8ac53c5c3b77c8ae36d346a7b
Alex Johnston <me@alexj.ca>
2025-11-07 05:42:51 -0700

    Work on blog roll

---
+++ src/lib/Util.ts
@@ -0,0 +1,8 @@
+export function formatDate(date: Date) {
+  // I really hate JavaScript some days.
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
+}
